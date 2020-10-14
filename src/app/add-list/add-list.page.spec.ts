b754f86ed60c643c3951a00b2aff6f9ea86cc69d
jgm139 <jgm139@alu.ua.es>
2020-10-14 15:26:37 +0200

    Refactorización de tabs

--- src/app/add-list/add-list.page.spec.ts
+++ src/app/add-list/add-list.page.spec.ts
@@ -2,19 +2,19 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { IonicModule } from '@ionic/angular';
 import { ExploreContainerComponentModule } from '../explore-container/explore-container.module';
 
-import { Tab2Page } from './tab2.page';
+import { AddListPage } from './add-list.page';
 
-describe('Tab2Page', () => {
-  let component: Tab2Page;
-  let fixture: ComponentFixture<Tab2Page>;
+describe('AddListPage', () => {
+  let component: AddListPage;
+  let fixture: ComponentFixture<AddListPage>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [Tab2Page],
+      declarations: [AddListPage],
       imports: [IonicModule.forRoot(), ExploreContainerComponentModule]
     }).compileComponents();
 
-    fixture = TestBed.createComponent(Tab2Page);
+    fixture = TestBed.createComponent(AddListPage);
     component = fixture.componentInstance;
     fixture.detectChanges();
   }));
---
+++ src/app/add-list/add-list.page.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-list',
+  templateUrl: 'add-list.page.html',
+  styleUrls: ['add-list.page.scss']
+})
+export class AddListPage {
+
+  constructor() {}
+
+}
